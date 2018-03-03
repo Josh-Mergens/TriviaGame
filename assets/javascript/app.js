7703ecd5655bc24b40a7fdc7b4bce22dc8178646
Josh Mergens <bluecatonarock@gmail.com>
2018-03-02 17:49:08 -0800

    commented steps to take in app.js, some updates to index.html

---
+++ assets/javascript/app.js
@@ -0,0 +1,26 @@
+// set up timer
+
+// timer start
+
+// timer stop
+
+// timer reset
+
+
+// choose question and append to trivialDefeat div
+
+// assign possible answers to chooserOrDie buttons
+
+// start timer
+
+// click events for choosing answers
+
+// check if chosen answer is correct or not
+
+// if answer is correct increment score and show victory message
+
+// if answer is false show defeat message
+
+// load next question and reset timer
+
+// all questions have been answered, show appropriate end message 
