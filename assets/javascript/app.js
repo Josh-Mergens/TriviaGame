665c2005b0f1b081fb22dd3503b08418b37fcd41
Josh Mergens <bluecatonarock@gmail.com>
2018-03-02 19:55:28 -0800

    created array of  trivia questions and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,28 @@
+// create trivia Questions
+var questionEverything = [{
+  {
+    what: 'Name a punk band that could also be called "Rooster Pugulist"?';
+    noChoice: ["Chicken Fighter", "Poultry Puncher", "Cock Sparrer", "Bird Boxer"];
+    oneChance: 3;
+  };
+  {
+    what: 'Which of the following is *not* considered to be amongst the first punk rock bands?';
+    noChoice: ["Velvet Underground", "The Clash", "New York Dolls", "The Stooges"];
+    oneChance:  2;
+  };
+  {
+    what: 'The song "Olympia WA" orignated from which of the following 90s punk bands?';
+    noChoice:["Rancid", "Greenday", "NOFX", "Pennywise"];
+    oneChance: 1;
+  };
+  {
+    what: '"Lee Ving on a Jet Plane", is a play on words using the name of an (in?)famous front man of one of the following late 70s - early 80s punk bands. Which band did he front?'
+    noChoice: ["The Adicts", "Agent Orange", "Fear", "Sham 69"]
+    oneChance: 3
+  }
+
+}];
+
 // set up timer
 
 // timer start
@@ -23,4 +48,4 @@
 
 // load next question and reset timer
 
-// all questions have been answered, show appropriate end message 
+// all questions have been answered, show appropriate end message
